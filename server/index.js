d57cb4fc22c7bb39ebb95852becefaf53b90b002
Flo <Florianparzefall@hotmail.de>
2021-10-25 12:30:01 +0100

    s

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 //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
-console.log("")
+console.log("s")
 console.log(process.env.DOCKER)
 const myExpress = require('./libs/express/main');
 const myImport = require('./libs/import/main');
